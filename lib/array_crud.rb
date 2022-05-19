3a76b6fadab92a3bbf1f97c62993e92e408896c9
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-05-19 11:56:43 -0700

    commit tree 7cfe3bac4e1071a5b151a834dc03a4e2ee230d1f

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,7 @@
+
+   
 def create_an_empty_array
-  arr = [] 
+  Array.new
 end
 
 def create_an_array
@@ -7,8 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  # array << element
-  array.push(element)
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
@@ -16,11 +17,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop()
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift()
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -28,7 +29,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[0]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
